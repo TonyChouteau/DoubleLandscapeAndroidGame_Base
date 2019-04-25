6bcca3dbfa7930a36863599106823e6b0e688f10
ElexXVII <34800257+ElexXVII@users.noreply.github.com>
2019-04-25 23:42:20 +0200

    Change (OnWindowsFocusChanged re-added)

--- main/java/fluffy/fluffydroid/MainActivity.java
+++ main/java/fluffy/fluffydroid/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    @Override
     protected void onStart() {
         Log.i("TEST","onStart()");
         super.onStart();
@@ -38,27 +40,33 @@ public class MainActivity extends Activity
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // Hide system bar and navigation bar
-        /*this.getWindow().getDecorView().setSystemUiVisibility(
+        this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);*/
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         myContext = this;
 
         setContentView(new GameView(this));
     }
+
+    @Override
     protected void onPause()
     {
         Log.i("TEST","onPause()");
         super.onPause();
     }
+
+    @Override
     protected void onStop() {
         Log.i("TEST","onStop()");
         super.onStop();
     }
+
+    @Override
     protected void onRestart() {
         Log.i("TEST","onRestart()");
         super.onRestart();
@@ -69,7 +77,7 @@ public class MainActivity extends Activity
     {
         if (hasFocus)
         {
-            Log.i("TEST","FocusChange");
+            // Hide system bar and navigation bar
             this.getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -79,5 +87,6 @@ public class MainActivity extends Activity
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
+
 }
 
